2f4df0fab9cec34bac1a7e140811e7ff9d3a18a7
joy <ciftekharulislam@gmail.com>
2021-07-11 07:43:06 -0700

    internal/index.js and dockerfile modified

--- internal/index.js
+++ internal/index.js
@@ -1,16 +1,22 @@
-'use strict';
+'use strict'
 
-const express = require('express');
+const { response } = require('express')
+const express = require('express')
 
 // Constants
-const PORT = 8080;
-const HOST = '0.0.0.0';
+const PORT = 8080
+const HOST = '0.0.0.0'
 
 // App
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+const app = express()
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+app.get('/', (request, response) => {
+  response.send('Internal server')
+})
+
+app.get('/api/v1/internal', (request, response) => {
+  response.send('Some data from internal server World')
+})
+
+app.listen(PORT, HOST)
+console.log(`Internal service running on http://${HOST}:${PORT}`)
